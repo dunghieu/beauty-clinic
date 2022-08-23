e783845202d8ff715969f981b325bcf66ef6c4d4
HieuLAPTOP <hieund.bhsoft@gmail.com>
2022-08-23 14:02:04 +0700

    init

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='flex flex'>
+      <div className='main-logo bg-logo bg-no-repeat h-16 w-64'></div>
+      <nav className='main-menu'></nav>
+    </header>
+  );
+};
+
+export default Header;
