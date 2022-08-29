b314ef5fa45ff754e69c5063f5053b375f605a25
HieuLAPTOP <hieund.bhsoft@gmail.com>
2022-08-29 09:56:55 +0700

    make it bedge

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import logo from '../../img/MainLogo.png';
 import Button from '../UI/Button';
 import Backdrop from '../UI/Backdrop';
@@ -7,11 +7,16 @@ import useWindowDimensions from '../../hooks/useWindowDimentions';
 import './Header.css';
 
 const Header = () => {
-  const { width } = useWindowDimensions();
+  const {width} = useWindowDimensions();
   const [isShow, setIsShow] = useState(false);
 
   const toggleMenu = (event) => {
     event.preventDefault();
+    if (!isShow) {
+      document.body.style.overflowY = 'hidden';
+    } else {
+      document.body.style.overflowY = 'auto';
+    }
     setIsShow((prevState) => !prevState);
   };
 
@@ -22,39 +27,41 @@ const Header = () => {
   }, [width, isShow]);
 
   return (
-    <header className="Header">
+    <header className='Header'>
       {isShow && <Backdrop />}
-      <div className="Header__wrapper">
-        <div className="Header__logo">
-          <img src={logo} alt="" />
+      <div className='Header__wrapper'>
+        <div className='Header__logo'>
+          <img src={logo} alt='' />
         </div>
-        <div className="Header__toggle" onClick={toggleMenu}>
-          <span className="Header__icon"></span>
+        <div className='Header__toggle' onClick={toggleMenu}>
+          <span className='Header__icon'></span>
         </div>
-        <div className={`Header__content ${isShow && width < 1024 ? 'show ' : ''}`}>
-          <div className="Header__menu ">
-            <ul className="Header__menu-children ">
+        <div
+          className={`Header__content ${isShow && width < 1024 ? 'show ' : ''}`}
+        >
+          <div className='Header__menu '>
+            <ul className='Header__menu-children '>
               <li>
-                <a href="#" className="active">
+                <a href='#' className='active'>
                   Home +
                 </a>
               </li>
               <li>
-                <a href="#">About</a>
+                <a href='#'>About</a>
               </li>
               <li>
-                <a href="#">Service</a>
+                <a href='#'>Service</a>
               </li>
               <li>
-                <a href="#">Gallery</a>
+                <a href='#'>Gallery</a>
               </li>
               <li>
-                <a href="#">Blog</a>
+                <a href='#'>Blog</a>
               </li>
             </ul>
           </div>
-          <div className="Header__contact">
-            <Button type="button">Contact</Button>
+          <div className='Header__contact'>
+            <Button type='button'>Contact</Button>
           </div>
         </div>
       </div>
